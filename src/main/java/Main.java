6b1a9e12464b1a5000d42fd792d1b5ffa1eabb5e
sofiacortez <sofia.cortez1509@gmail.com>
2021-10-14 12:45:39 -0300

    Entrega final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Main {
+import controllers.HomeController;
+import controllers.SaludoController;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+import spark.servlet.SparkApplication;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+public class Main {
+  public static void main(String[] args) {
+    Spark.port(9000);
+    HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+
+    HomeController home = new HomeController();
+    SaludoController saludo = new SaludoController();
+
+    Spark.get("/", home::index);
+    Spark.get("/bienvenida", saludo::bienvenida, engine);
+    Spark.get("/despedida", saludo::despedida, engine);
+  }
 }
+
+//public class Main {
+//  public static void main(String[] args) {
+//    Spark.port(9000);
+//    Spark.get("/", (request, response) -> "<h1>Hola " + request.queryParamOrDefault("nombre", "Nini") + "!</h1>"
+//                                              + "<p>Mamonshiti no ta</p>");
+//  }
+//}
--- src/main/java/controllers/SaludoController.java
+++ src/main/java/controllers/SaludoController.java
@@ -1,2 +1,18 @@
-package controllers;public class SaludoController {
+package controllers;
+
+import spark.ModelAndView;
+import spark.Response;
+import spark.Request;
+
+public class SaludoController {
+  public ModelAndView bienvenida(Request request, Response response) {
+    return new ModelAndView(request.queryParamOrDefault("nombre", "Nini"),
+                                              "bienvenida.html.hbs");
+  }
+
+  public ModelAndView despedida(Request request, Response response) {
+    return new ModelAndView(request.queryParamOrDefault("nombre", "Nini"),
+        "despedida.html.hbs");
+  }
+
 }
